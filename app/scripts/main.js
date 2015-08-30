f92e32a578abba37d098dec569c9014173efb6a5
Amie Wilt <amie.wilt@gmail.com>
2015-08-30 16:52:59 -0700

    deleted pages files, added logo container partial, installed gsap, worked on nav item animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,18 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+
+$(document).ready(function () {
+  //Slide nav list items from primary nav in from the right or bottom on load
+
+  //Slide nav list banners on sections from left on scroll
+
+  //Smooth scroll and stop at each section
+
+  //$('.primary-nav-list-item').animate({ right: 0 });
+
+
+
+
+});
+
+
+TweenMax.staggerFrom('.primary-nav-list-item', 0.3, {right: -400, delay: 0.2}, 0.1);
